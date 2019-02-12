7773c961c30dd33697971012af7b682cbf9963e5
Alex Leska <deskmicealex@gmail.com>
2019-02-11 23:14:13 -0600

    gh-pages deployment added

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
-      }
-    ]
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+    ],
   },
-  plugins: [new ExtractTextPlugin("css/mystyles.css")]
+  plugins: [new ExtractTextPlugin('css/mystyles.css')],
 };
